43efed367ee919e18355cc60a47e3d929b90936b
Kausinho <kausinhobrunow@gmail.com>
2021-03-17 13:06:30 -0300

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -129,11 +129,67 @@ $(function(){
 
     /*Direcionamento para a div contato usando o atributo goto*/
 
+    var directory = '/projeto_05/'
+
     $('[goto=contato]').click(function(){
-        $('nav a').css('color','black');
-        $(this).css('color','#EB2D2D');
-        $('html,body').animate({'scrollTop':$('#contato').offset().top},1000);
+        location.href=directory+'index.html?contato';
        return false; 
     });
+
+    checkUrl();
+
+    function checkUrl(){
+        var url = location.href.split('/');
+        var curPage = url[url.length-1].split('?');
+
+        if(curPage[1] != undefined && curPage[1] == 'contato'){
+            $('header nav a').css('color','black');
+            $('footer nav a').css('color','white');
+            $('[goto=contato]').css('color','EB2D2D');
+            $('html,body').animate({'scrollTop':$('#contato').offset().top});
+        }
+    }
+
+
+    /*Menu Responsivo*/
+
+    $('.mobile').click(function(){
+        $(this).find('ul').slideToggle();
+    });
+
+
+    /*Sistema de navegação nos depoimentos da index.html*/
+
+    var amtDepoimento = $('.depoimento-single p').length;
+    var curIndex = 0;
+
+    iniciarDepoimentos();
+    navegarDepoimentos();
+
+    function iniciarDepoimentos(){
+        $('.depoimento-single p').hide();
+        $('.depoimento-single p').eq(0).show();
+    }
+
+    function navegarDepoimentos(){
+        $('[next]').click(function(){
+                curIndex++;
+                if(curIndex >= amtDepoimento){
+                    curIndex = 0;
+                }
+            $('.depoimento-single p').hide();
+            $('.depoimento-single p').eq(curIndex).show();
+        });
+
+        $('[prev]').click(function(){
+            curIndex--;
+                if(curIndex < 0){
+                    curIndex = amtDepoimento-1;
+                }
+            $('.depoimento-single p').hide();
+            $('.depoimento-single p').eq(curIndex).show();
+        });
+    }
+
 
 });
